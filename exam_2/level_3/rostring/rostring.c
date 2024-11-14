e7d11f87ff9f54f84964c731c317a98e584710dc
Edouard Proust <contact@edouardproust.dev>
2024-11-14 21:17:22 +0100

    rostring

--- exam_2/level_3/rostring/rostring.c
+++ exam_2/level_3/rostring/rostring.c
@@ -5,18 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eproust <contact@edouardproust.dev>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 18:03:45 by eproust           #+#    #+#             */
-/*   Updated: 2024/11/12 20:38:34 by eproust          ###   ########.fr       */
+/*   Created: 2024/11/14 20:22:28 by eproust           #+#    #+#             */
+/*   Updated: 2024/11/14 21:16:56 by eproust          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
-
-int	ft_isspace(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\v');
-}
 
 int	ft_strlen(char *s)
 {
@@ -28,93 +22,65 @@ int	ft_strlen(char *s)
 	return (i);
 }
 
-char	*get_start(char *s)
+int	issp(char c)
 {
-	while (*s && ft_isspace(*s))
+	return (c == '\0' || c == ' ' || c == '\t' || c == '\v');
+}
+
+char	*first_word(char *s)
+{
+	while (*s && issp(*s))
 		s++;
 	return (s);
 }
 
-char	*get_first_word(char *s)
+char	*second_word(char *s)
 {
-	char	*first;
-
-	while (*s && ft_isspace(*s))
+	s = first_word(s);
+	while (*s && !issp(*s))
 		s++;
-	first = s;
-	while (*s && !ft_isspace(*s))
-		s++;
-	while (*s && ft_isspace(*s))
-		s++;
-	if (*s != '\0')
-		first = s;
-	return (first);
+	return (first_word(s));
 }
 
-char	*copy_trim(char *s)
+void	print_word(char *s)
 {
-	char	*cpy;
-	char	*st;
-	char	*fi;
-	int		i;
-	int		j;
-
-	cpy = malloc((ft_strlen(s) + 1) * sizeof(char));
-	if (!cpy)
-		return (NULL);
-	st = get_start(s);
-	fi = get_first_word(s);
-	i = 0;
-	if (st == fi)
-	{
-		while (fi[i] && !ft_isspace(fi[i]))
-		{
-			cpy[i] = fi[i];
-			i++;
-		}
-		return (cpy);
-	}
-	j = 0;
-	while (fi[i])
-	{
-		if (!ft_isspace(fi[i]) || (ft_isspace(fi[i]) && i > 0 && !ft_isspace(fi[i - 1])))
-			cpy[j++] = fi[i];
-		i++;
-	}
-	if (cpy[j - 1] != ' ')
-	{
-		cpy[j] = ' ';
-		j++;
-	}
-	while (!ft_isspace(*st))
-		cpy[j++] = *st++;
-	return (cpy);
+	while (*s && !issp(*s))
+		write(1, s++, 1);
 }
 
-char	*rostring(char *s)
+void	rostring(char *s)
 {
 	char	*out;
+	int		len;
+	char	*fir_word;
+	char	*sec_word;
 
-	out = copy_trim(s);
+	len = ft_strlen(s);
+	out = malloc(sizeof(char) * (len + 1));
 	if (!out)
-		return (NULL);
-	return (out);
+		return ;
+	fir_word = first_word(s);
+	sec_word = second_word(s);
+	if (*sec_word == '\0')
+		print_word(fir_word);
+	else
+	{	
+		while (*sec_word)
+		{
+			if (!issp(*sec_word))
+				write(1, sec_word, 1);
+			if (!issp(*sec_word) && issp(*(sec_word + 1)))
+				write(1, " ", 1);
+			sec_word++;
+		}
+		print_word(fir_word);
+	}
 }
 
 int	main(int ac, char **av)
 {
-	char	*out;
-	int		i;
-
 	if (ac > 1)
-	{
-		out = rostring(av[1]);
-		if (!out)
-			return (1);
-		i = 0;
-		while (out[i])
-			write(1, &out[i++], 1);
-		free(out);	
-	}
+		rostring(av[1]);
 	write(1, "\n", 1);
+	return (0);
 }

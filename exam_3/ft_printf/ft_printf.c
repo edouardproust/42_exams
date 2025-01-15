491b6002a10466605cc5af21d4bd1232b713bd1e
Edouard Proust <contact@edouardproust.dev>
2025-01-15 15:01:30 +0100

    ft_printf

--- exam_3/ft_printf/ft_printf.c
+++ exam_3/ft_printf/ft_printf.c
@@ -5,39 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eproust <contact@edouardproust.dev>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 18:08:24 by eproust           #+#    #+#             */
-/*   Updated: 2025/01/15 14:21:45 by eproust          ###   ########.fr       */
+/*   Created: 2025/01/15 14:27:24 by eproust           #+#    #+#             */
+/*   Updated: 2025/01/15 14:58:04 by eproust          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
 
-static int	put_string(const char *str)
+static int	put_string(char *s)
 {
-	size_t	written;
+	int	written;
 
 	written = 0;
-	if (str == NULL)
+	if (s == NULL)
 		written = write(1, "(null)", 6);
 	else
 	{
-		while (*str)
-			written += write(1, str++, 1);
+		while (*s)
+			written += write(1, s++, 1);
 	}
 	return (written);
 }
 
 static int	put_decimal(int n, int written)
 {
-	long	nl;
 	char	c;
+	long	nl;
 
 	nl = (long)n;
 	if (nl < 0)
 	{
-		nl = -nl;
 		written += write(1, "-", 1);
+		nl = -nl;
 	}
 	if (nl >= 10)
 		written = put_decimal(nl / 10, written);
@@ -59,38 +59,38 @@ static int	put_hexa(unsigned int n, int written)
 
 static int	put_format(va_list ap, char c)
 {
-	int		written;
-	char	*s;
-	int		d;
-	int		x;
+	int				written;
+	char			*s;
+	int				d;
+	unsigned int	x;
 
 	written = 0;
 	if (c == 's')
 	{
 		s = va_arg(ap, char *);
-		written += put_string(s);
+		written = put_string(s);
 	}
 	else if (c == 'd')
 	{
 		d = va_arg(ap, int);
-		written += put_decimal(d, 0);
+		written = put_decimal(d, 0);
 	}
 	else if (c == 'x')
 	{
-		x = va_arg(ap, int);
-		written += put_hexa(x, 0);
+		x = va_arg(ap, unsigned int);
+		written = put_hexa(x, 0);
 	}
 	return (written);
 }
 
 int	ft_printf(const char *fmt, ...)
 {
-	int		written;
 	va_list	ap;
+	int		written;
 	int		flag;
 
-	written = 0;
 	va_start(ap, fmt);
+	written = 0;
 	flag = 0;
 	while (*fmt)
 	{
@@ -114,18 +114,16 @@ int	ft_printf(const char *fmt, ...)
 
 int	main(void)
 {
-	int	written = 0;
-	char *s = NULL;
+	char *s1 = NULL;
+	char *s2 = "Hello world!";
 	int	d = INT_MIN;
-	int x = UINT_MAX;
+	int	x = UINT_MAX;
+	int	res = 0;
 
-	written = ft_printf("string: '%s', int: '%d', hexa: '%x'\n", s, d, x);
-	printf("written (ft_printf): %d\n", written);
-	written = printf("string: '%s', int: '%d', hexa: '%x'\n", s, d, x);
-	printf("written (printf): %d\n", written);
-	written = ft_printf("%s\n", "toto");
-	written = ft_printf("Magic %s is %d\n", "number", 42);
-	written = ft_printf("Hexadecimal for %d is %x\n", 42, 42);
+	res = ft_printf("s1: '%s', s2: '%s', int: '%d', hexa: '%x'\n", s1, s2, d, x);
+	printf("written (ft_printf): %d\n", res);
+	res = printf("s1: '%s', s2: '%s', int: '%d', hexa: '%x'\n", s1, s2, d, x);
+	printf("written (printf): %d\n", res);
 	return (0);
 }
 */

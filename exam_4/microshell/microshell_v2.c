38a2d7eda38de521df75b1df5680cf63f203da50
Edouard Proust <contact@edouardproust.dev>
2025-05-10 18:35:16 +0200

    microshell v2

--- exam_4/microshell/microshell_v2.c
+++ exam_4/microshell/microshell_v2.c
@@ -13,7 +13,16 @@ static void	put_error(char *err, char *arg)
 	write(STDERR_FILENO, "\n", 1);
 }
 
-static void	run_cd(char **argv, int i)
+static int	next_sep_index(char **argv)
+{
+	int	i = 0;
+
+	while (argv[i] && strcmp("|", argv[i]) && strcmp(";", argv[i]))
+		i++;
+	return (i);
+}
+
+static void	execute_cd(char **argv, int i)
 {
 	if (i != 2)
 		put_error("error: cd: bad arguments", NULL);
@@ -21,54 +30,56 @@ static void	run_cd(char **argv, int i)
 		put_error("error: cd: cannot change directory to ", argv[1]);
 }
 
-static void	set_pipe(int *fds, int end)
+static void	set_pipe_if(int has_pipe, int *fds, int end)
 {
+	if (!has_pipe)
+		return ;
 	dup2(fds[end], end);
 	close(fds[1]);
 	close(fds[0]);
 }
 
-
-static void	execute_cmd(char **argv, int i, char **envp)
+static void	execute_cmd(char **argv, int i, char **envp, int saved_stdin)
 {
-	int		has_pipe;
+	int		has_pipe = argv[i] && !strcmp("|", argv[i]);
+	int		has_semicolon = argv[i] && !strcmp(";", argv[i]);
 	int		fds[2];
 	pid_t	pid;
-	int		status;
 
-	if (!strcmp("cd", argv[0]))
-		run_cd(argv, i);
-	has_pipe = argv[i] && !strcmp("|", argv[i]);
 	if (has_pipe)
 		pipe(fds);
 	pid = fork();
 	if (pid == 0)
 	{
-		if (has_pipe)
-			set_pipe(fds, STDOUT_FILENO);
+		set_pipe_if(has_pipe, fds, STDOUT_FILENO);
 		argv[i] = NULL;
 		execve(argv[0], argv, envp);
 		put_error("error: cannot execute ", argv[0]);
 		exit (EXIT_FAILURE);
 	}
-	waitpid(pid, &status, 0);
-	if (has_pipe)
-		set_pipe(fds, STDIN_FILENO);
+	waitpid(pid, NULL, 0);
+	set_pipe_if(has_pipe, fds, STDIN_FILENO);
+	if (has_semicolon)
+		dup2(saved_stdin, STDIN_FILENO);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	i;
+	int	i = 0;
+	int	saved_stdin = dup(STDIN_FILENO);
 
 	(void)argc;
-	i = 0;
 	while (argv[i])
 	{
 		argv += i + 1;
-		while (argv[i] && strcmp("|", argv[i]) && strcmp(";", argv[i]))
-			i++;
+		i = next_sep_index(argv);
 		if (i != 0)
-			execute_cmd(argv, i, envp);
+		{
+			if (!strcmp("cd", argv[0]))
+				execute_cd(argv, i);
+			else
+				execute_cmd(argv, i, envp, saved_stdin);
+		}
 	}
 	return (EXIT_SUCCESS);
 }

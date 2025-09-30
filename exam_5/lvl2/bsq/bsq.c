79b2a35844314d9ccedd12f76d887d88c91e8970
Edouard Proust <contact@edouardproust.dev>
2025-09-30 04:06:15 +0200

    bsq

--- exam_5/lvl2/bsq/bsq.c
+++ exam_5/lvl2/bsq/bsq.c
@@ -21,81 +21,76 @@ int	ft_min3(int a, int b, int c) {
 
 void	free_grid_char(char** grid, int n) {
 	if (!grid) return;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		if (grid[i]) free(grid[i]);
-	}
 	free(grid);
 }
 
 void	free_grid_int(int** grid, int n) {
 	if (!grid) return;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		if (grid[i]) free(grid[i]);
-	}
 	free(grid);
 }
 
 int	check_line(char* line, Map* m, size_t len) {
-	int i = 0;
-	while (line[i]) {
+	if (len != (size_t)m->cols) return 0;
+	for (int i = 0; line[i]; i++) {
 		char c = line[i];
 		if (c != m->empt && c != m->obst) return 0;
-		i++;
 	}
-	if (len != (size_t)m->cols) return 0;
 	return 1;
 }
 
 char**	parse_grid(FILE* f, Map* m) {
-	char** grid = malloc(m->rows * sizeof(char*)); // TODO failure check
+	char** grid = malloc(m->rows * sizeof(char*));
+	if (!grid) return NULL;
 	int y = 0;
 	while (y < m->rows) {
-		char* line = NULL;
-		size_t n = 0;
-		size_t len = getline(&line, &n, f); // TODO failure check
+		char* line = NULL; //!\ Must be initialized to 0
+		size_t n = 0; //!\ Idem
+		ssize_t len = getline(&line, &n, f);
+		if (len == -1)
+			return (free(line), free_grid_char(grid, y), NULL); //!\ Don't forget free(line)
 		if (line[len - 1] == '\n') {
 			len--;
 			line[len] = '\0';
 		}
 		if (y == 0) m->cols = len;
-		if (!check_line(line, m, len)) {
-			free(line);
-			free_grid_char(grid, y);
-			return NULL;
-		}
-		grid[y++] = line;
+		if (!check_line(line, m, len))
+			return (free(line), free_grid_char(grid, y), NULL); //!\ Idem
+		grid[y++] = line; //!\ line is allocated by getline()
 	}
-	if (y != m->rows) {
-		free_grid_char(grid, m->rows);
-		return NULL;
-	}
+	if (y != m->rows)
+		return (free_grid_char(grid, y), NULL);
 	return grid;
 }
 
 Map*	parse_map(FILE* f) {
-	Map* m = calloc(1, sizeof(Map)); // TODO failure check
+	Map* m = calloc(1, sizeof(Map));
+	if (!m) return NULL;
 	if ((fscanf(f, "%d %c %c %c\n", &m->rows, &m->empt, &m->obst, &m->full) != 4)
-		|| (m->empt == m->obst || m->empt == m->full || m->obst == m->full)) {
-		free(m);
-		return NULL;
-	}
+		|| (m->empt == m->obst || m->empt == m->full || m->obst == m->full))
+			return (free(m), NULL);
 	m->grid = parse_grid(f, m);
-	if (!m->grid) {
-		free(m);
-		return NULL;
-	}
+	if (!m->grid)
+		return (free(m), NULL);
 	return m;
 }
 
 void	solve_bsq(Map* m) {
-	int** tmp = malloc(m->rows * sizeof(int*)); // TODO failure check
+	int** tmp = malloc(m->rows * sizeof(int*));
+	if (!tmp)
+		return ft_error("error");
 	int side_len = 0, start_x = 0, start_y = 0;
 	for (int y = 0; y < m->rows; y++) {
-		tmp[y] = calloc(m->cols, sizeof(int)); // TODO failure check
+		tmp[y] = malloc(m->cols * sizeof(int)); //!\ Allocate row
+		if (!tmp[y])
+			return (ft_error("error"), free_grid_int(tmp, y));
 		for (int x = 0; x < m->cols; x++) {
 			if (m->grid[y][x] == m->obst) tmp[y][x] = 0;
 			else if (y == 0 || x == 0) tmp[y][x] = 1;
-			else tmp[y][x] = ft_min3(tmp[y-1][x], tmp[y][x-1], tmp[y-1][x-1]) + 1;
+			else tmp[y][x] = ft_min3(tmp[y-1][x], tmp[y][x-1], tmp[y-1][x-1]) + 1; //!\ Add 1
 			if (tmp[y][x] > side_len) {
 				side_len = tmp[y][x];
 				start_x = x - side_len + 1;
@@ -103,7 +98,7 @@ void	solve_bsq(Map* m) {
 			}
 		}
 	}
-	// Draw square on map
+	// Draw BSQ on grid
 	for (int y = start_y; y < start_y + side_len; y++) {
 		for (int x = start_x; x < start_x + side_len; x++) {
 			m->grid[y][x] = m->full;
@@ -114,23 +109,15 @@ void	solve_bsq(Map* m) {
 }
 
 void	print_bsq(Map* m) {
-	for (int y = 0; y < m->rows; y++) {
+	for (int y = 0; y < m->rows; y++)
 		fprintf(stdout, "%s\n", m->grid[y]);
-	}
 }
 
 void process_file(char* filename) {
 	FILE* f = filename ? fopen(filename, "r") : stdin;
-	if (!f) {
-		ft_error("map error");
-		return;
-	}
+	if (!f) return ft_error("map error");
 	Map* m = parse_map(f);
-	if (!m) {
-		ft_error("map error");
-		ft_close(f);
-		return;
-	}
+	if (!m) return (ft_error("map error"), ft_close(f));
 	solve_bsq(m);
 	print_bsq(m);
 	// Cleanup
@@ -147,4 +134,5 @@ int	main(int ac, char** av) {
 			process_file(av[i]);
 		}
 	}
+	return 0;
 }
--- exam_5/lvl2/life/life.c
+++ exam_5/lvl2/life/life.c
@@ -2,62 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	ft_error() {
-	char* msg = "error\n";
-	for (int i = 0; msg[i]; i++) {
-		putchar(msg[i]);
-	}
-	return 1;
-}
-
 int main(int ac, char** av) {
-	if (ac != 4) {
-		return ft_error();
-	}
-	int w = atoi(av[1]), h = atoi(av[2]), iter = atoi(av[3]);
+	if (ac != 4) return 1;
+	// Init vars and board
+	int x = 0, y = 0, is_writing = 0;
+	int w = atoi(av[1]), h = atoi(av[2]), iters = atoi(av[3]);
 	int board[h][w], next[h][w];
-	// Init board
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
 			board[y][x] = 0;
 		}
 	}
-
 	// Read input
-	char buf[1024];
-	int bytes_read, y = 0, x = 0, is_writing = 0;
-	while ((bytes_read = read(0, buf, 1024)) > 0) {
-		for (int i = 0; i < bytes_read; i++) {
-			if (buf[i] == 'w' && y > 0) y--;
-			else if (buf[i] == 'a' && x > 0) x--;
-			else if (buf[i] == 's' && y < h - 1) y++;
-			else if (buf[i] == 'd' && x < w - 1) x++;
-			else if (buf[i] == 'x') is_writing = !is_writing;
-			if (is_writing) board[y][x] = 1;
-		}
+	char c;
+	while (read(0, &c, 1) > 0) {
+		if (c == 'w' && y > 0) y--;
+		else if (c == 'a' && x > 0) x--;
+		else if (c == 's' && y < h-1) y++;
+		else if (c == 'd' && x < w-1) x++;
+		else if (c == 'x') is_writing = !is_writing;
+		else if (c != '\n') return 1;
+		if (is_writing) board[y][x] = 1;
 	}
 	// Iterations
-	for (int i = 0; i < iter; i++) {
+	for (int i = 0; i < iters; i++) {
 		for (int y = 0; y < h; y++) {
 			for (int x = 0; x < w; x++) {
 				int n = 0;
 				for (int dy = -1; dy <= 1; dy++) {
 					for (int dx = -1; dx <= 1; dx++) {
-						if ((x + dx >= 0) && (x + dx < w) && (y + dy >= 0) && (y + dy < h)) {
-							if (board[y + dy][x + dx] == 1) n++;	
-						}
+						n += (dy || dx) && (y + dy >= 0) && (x + dx >= 0) && (y + dy < h) && (x + dx < w) && board[y + dy][x + dx];
 					}
 				}
-				// Life conditions
-				if (board[y][x] == 1) {
-					n--; // important
-					if (n == 2 || n == 3) next[y][x] = 1;
-					else next[y][x] = 0;	
-				} else if (n == 3) {
-					next[y][x] = 1;
-				} else {
-					next[y][x] = 0;
-				}
+				next[y][x] = (board[y][x] && (n == 2 || n == 3)) || (!board[y][x] && n == 3); 
 			}
 		}
 		// Copy next on board
@@ -67,14 +44,12 @@ int main(int ac, char** av) {
 			}
 		}
 	}
-	// print result
+	// Print result
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
-			if (board[y][x] == 0) putchar(' ');
-			else if (board[y][x] == 1) putchar('0');
+			putchar(board[y][x] ? '0': ' ');
 		}
 		putchar('\n');
 	}
-
 	return 0;
 }
